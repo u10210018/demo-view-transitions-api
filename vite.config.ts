255b9bba119f63907421320d07ea974522729c34
u10210018 <69283233+u10210018@users.noreply.github.com>
2023-11-27 19:29:03 +0800

    update yml

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: '/demo-view-transitions-api/',
     plugins: [
       vue(),
       AutoImport({
